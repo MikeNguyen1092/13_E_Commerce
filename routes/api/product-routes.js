9c88bec8c20a23263ebbf623855ed17d9011cdf8
Mike <nguyen.mike1092@gmail.com>
2024-02-05 20:31:38 -0500

    tag get and id finished

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -24,7 +24,7 @@ router.get("/:id", async (req, res) => {
 			include: [{ model: Category, model: Tag }],
 		});
 		if (!productData) {
-			res.status(404).json({ message: "No category found with that id!" });
+			res.status(404).json({ message: "No product found with that id!" });
 			return;
 		}
 		res.status(200).json(productData);
